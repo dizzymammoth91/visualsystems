e11c819c021c1b0313f0070abcee2d299ba7c894
dizzymammoth91 <lyntonsutton@googlemail.com>
2026-02-13 19:03:47 +0000

    Add files via upload

---
+++ Lab2-Colour-Perception-main/Lab2.m
@@ -0,0 +1,29 @@
+imfinfo('peppers.png')
+%%
+RGB = imread('peppers.png');  
+imshow(RGB)
+
+%% convert to greyscale
+ 
+I = rgb2gray(RGB);
+figure              % start a new figure window
+imshow(I)
+
+%% show images side by side
+
+imshowpair(RGB, I, 'montage')
+title('Original colour image (left) grayscale image (right)');
+
+%% split image into seperate chanels
+[R,G,B] = imsplit(RGB);
+montage({R, G, B},'Size',[1 3])
+
+%% Map RGB image to HSV space and into separate channels
+HSV = rgb2hsv(RGB);
+[H, S, V] = imsplit(HSV);
+montage({H,S,V},'Size', [1 3])
+
+%% Map RGB image to XYZ space
+XYZ = rgb2xyz(RGB)
+[X, Y, Z] = imsplit(XYZ);
+montage({X,Y,Z},'Size', [1 3])
